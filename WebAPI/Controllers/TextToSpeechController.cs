496487a251b78e176eeeed2a15cbc7a3465a9973
Luiz Henrique De Jesus Correia <38606125814@info.local>
2024-04-24 10:54:48 -0300

    save

---
+++ WebAPI/Controllers/TextToSpeechController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextToSpeechController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Main(string text)
+        {
+            try
+            {
+                var speechKey = "9d1b809846f5428e93c3a3d12500d8a0";
+                var speechRegion = "speechRegion";
+
+                var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+
+                // The neural multilingual voice can speak different languages based on the input text.
+                speechConfig.SpeechSynthesisVoiceName = "en-US-AvaMultilingualNeural";
+
+                using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+                {
+                    var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text);
+                    return Ok(speechSynthesisResult);
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+        }
+    }
+}
--- WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc17027bc33b83ddf509e1d8945099f2788208f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af839dd5f39ff623fb07e7a1139b9644789e698")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
